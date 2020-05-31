e7f0fc50b9c895c8650a13ed0400c7ca17ad8b8c
子宜 <polarbear0512@gmail.com>
2020-06-01 01:09:03 +0800

    in waiting

--- communication.py
+++ communication.py
@@ -8,6 +8,15 @@ args = parser.parse_args()
 
 arduino = serial.Serial(args.serial_port, 9600, timeout=.1)
 while True:
+    
+    bytesOnBuffer = arduino.in_waiting
+    print("Bytes available: {}".format(bytesOnBuffer))
+
+    if bytesOnBuffer > 0:
+        data = arduino.readline().decode('utf-8')
+        print(data)
+
+    """
     data = arduino.readline()[:-2].decode("utf-8")  # the last bit gets rid of the new-line chars
     if data:
-        print(data)
+        print(data)"""
