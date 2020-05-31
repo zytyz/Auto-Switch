edf801e8f550a9eef789d2c94dc7c9fd73138838
子宜 <polarbear0512@gmail.com>
2020-06-01 01:12:58 +0800

    in waiting

--- communication.py
+++ communication.py
@@ -1,20 +1,18 @@
 import serial
 import argparse
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-sp', '--serial_port', default="/dev/cu.usbmodem142201", help="Serial Port Name")
 args = parser.parse_args()
 
 arduino = serial.Serial(args.serial_port, 9600, timeout=.1)
 while True:
-    
     bytesOnBuffer = arduino.in_waiting
-    print("Bytes available: {}".format(bytesOnBuffer))
+    # print("Bytes available: {}".format(bytesOnBuffer))
 
-    if bytesOnBuffer > 0:
-        data = arduino.readline().decode('utf-8')
-        print(data)
+    # if bytesOnBuffer > 0:
+    data = arduino.read(2)
+    print(data)
 
     """
     data = arduino.readline()[:-2].decode("utf-8")  # the last bit gets rid of the new-line chars
